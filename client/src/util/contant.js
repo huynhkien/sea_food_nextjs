1d2a8c3bd5ff055ba88bfff4ce3c912c2bcabb29
huynhkien <huynhhuukien2962@gmail.com>
2024-12-21 12:00:27 +0700

    util

---
+++ client/src/util/contant.js
@@ -0,0 +1,91 @@
+export const sorts = [
+    {
+        id: 1,
+        value: '-sold',
+        text: 'Best Selling'
+    },
+    {
+        id: 2,
+        value: '-name',
+        text: 'Alphabetically, A-Z'
+    },
+    {
+        id: 3,
+        value: 'name',
+        text: 'Alphabetically, Z-A'
+    },
+    {
+        id: 4,
+        value: '-price',
+        text: 'Price, high to low'
+    },
+    {
+        id: 5,
+        value: 'price',
+        text: 'Price, low to hight'
+    },
+    {
+        id: 6,
+        value: '-createdAt',
+        text: 'Date, new to old'
+    },
+    {
+        id: 7,
+        value: 'createdAt',
+        text: 'Date, old to new'
+    },
+]
+export const roles =[
+    {
+        code: 2002,
+        value: 'Admin'
+    },
+    {
+        code: 2004,
+        value: 'User'
+    },
+]
+
+export const blockStatus =[
+    {
+        code: true,
+        value: 'Blocked'
+    },
+    {
+        code: false,
+        value: 'Active'
+    },
+]
+
+export const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+export const voteOptions = [
+    {
+        id: 1,
+        text: 'Siêu tệ'
+    },
+    {
+        id: 2,
+        text: 'Tệ'
+    },
+    {
+        id: 3,
+        text: 'Trung bình'
+    },
+    {
+        id: 4,
+        text: 'Tốt'
+    },
+    {
+        id: 5,
+        text: 'Tuyệt vời'
+    },
+]
